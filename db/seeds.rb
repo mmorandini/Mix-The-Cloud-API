933bc3c59bf7a10e08bbcc3f9b3bf60bfe0546a7
mmorandini <emmemorandini@gmail.com>
2017-06-01 18:02:30 +0100

    added user model

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,4 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+u1 = User.create!(username: "matteo", first_name: "Matteo", last_name: "Smith", email: "matteo@matteo.it")
